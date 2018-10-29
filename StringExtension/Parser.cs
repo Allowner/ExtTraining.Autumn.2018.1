8e2160c12506f5e7fef98470e406a03af41bace5
Allowner <se1999va@tut.by>
2018-10-30 01:34:42 +0300

    Task 2

--- StringExtension/Parser.cs
+++ StringExtension/Parser.cs
@@ -10,7 +10,45 @@ namespace StringExtension
     {
         public static int ToDecimal(this string source, int @base)
         {
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException("Source string is null");
+            }
+
+            if (@base > 16 || @base < 2)
+            {
+                throw new ArgumentOutOfRangeException("Base out of range");
+            }
+
+            string upperSourse = source.ToUpperInvariant();
+            int power = 1;
+            int @decimal = 0;
+            int decimalIncrement;
+            for (int i = upperSourse.Length - 1; i >= 0; --i)
+            {
+                if (CharToInt(upperSourse[i]) >= @base)
+                {
+                    throw new ArgumentException("Wrong base");
+                }
+
+                decimalIncrement = CharToInt(upperSourse[i]) * power;
+                if(int.MaxValue - decimalIncrement < @decimal)
+                {
+                    throw new ArgumentException("Too big value");
+                }
+                @decimal += decimalIncrement;
+                power = power * @base;
+            }
+
+            return @decimal;
+        }
+
+        private static int CharToInt(char c)
+        {
+            if (c >= '0' && c <= '9')
+                return (int)c - '0';
+            else
+                return (int)c - 'A' + 10;
         }
     }
 }
